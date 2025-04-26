b2f438a4a4f454e8e4bfc43fde61e42ede2e4f7d
chenxudong2020 <872603935@qq.com>
2025-04-26 17:09:57 +0800

    fix数据库

--- rootfs/etc/cont-init.d/00-init.sh
+++ rootfs/etc/cont-init.d/00-init.sh
@@ -11,14 +11,9 @@ else
 fi
 
 CONTAINER_DB_DIR="/var/lib/headscale"
-DB_FILE="$CONTAINER_DB_DIR/db.sqlite"
-if [ ! -f "$DB_FILE" ]; then
-	echo "将自动生成数据库文件"
-	# 初始化数据库
-    cd /app
-	python3 -m flask db init
-	python3 -m flask db migrate
-	python3 -m flask db upgrade
+if [ -z "$(ls -A $CONTAINER_DB_DIR 2>/dev/null)" ]; then
+	echo "将自动复制数据库文件"
+	cp -r $INIT_DATA_APP_CONFIG/var /
 else
     echo "检测到SQLITE已有数据"
 fi
